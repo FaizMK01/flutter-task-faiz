98040efbf8eb01f996b34b444b0ac9c6681bec81
Faiz MK <faizmk@Faiz.local>
2026-02-25 15:01:52 +0500

    Configure stacked architecture

---
+++ lib/app/app.dart
@@ -0,0 +1,33 @@
+import 'package:faiztask/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:faiztask/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:faiztask/ui/views/home/home_view.dart';
+import 'package:faiztask/ui/views/home/home_viewmodel.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:faiztask/ui/bottom_sheets/cart/cart_sheet.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    // @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    LazySingleton(classType: HomeViewModel), // ← add karo
+    // @stacked-service
+  ],
+  bottomsheets: [
+   // StackedBottomsheet(classType: NoticeSheet),
+    StackedBottomsheet(classType: CartSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+   // StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
